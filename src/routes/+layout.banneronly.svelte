44091afbc967bcb8b9a9fe7725cda8b20deed298
0xnoid <efg@mimmikk.com>
2025-04-14 17:37:32 +0200

    Updated examples

---
+++ src/routes/+layout.banneronly.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Consentbanner from '$lib/components/consentBanner.svelte';
+</script>
+
+<svelte:head>
+    <title>Your Website</title>
+</svelte:head>
+
+<Consentbanner />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,34 @@
 <script>
     import Consentbanner from '$lib/components/consentBanner.svelte';
+    import { consentStore } from '$lib/stores/consentStore';
+
+    // Analytics
+    $: analyticsConsented = $consentStore.analytics === 'accepted';
+    
+    // Third Party
+    $: thirdPartyConsented = $consentStore.thirdParty === 'accepted';
+    
+    // Any status (for Required items)
+    $: anyConsented = analyticsConsented || thirdPartyConsented;
 </script>
 
 <svelte:head>
-    <title>Your Website</title>
+    <!-- Regular: -->
+    <script>always loaded script here</script>
+    <!-- Load as long as there's any consent status (e.g. required, useful for legalities) -->
+    {#if anyConsented}
+        <script>script for users who accepted something</script>
+    {:else}
+        <script>alternative script for users who declined everything</script>
+    {/if}
+    {#if analyticsConsented}
+      <!-- Analytics scripts -->
+      <script>analytics scripts here</script>
+    {/if}
+    {#if thirdPartyConsented}
+      <!-- Third party scripts -->
+      <script>third party scripts here</script>
+    {/if}
 </svelte:head>
 
 <Consentbanner />
